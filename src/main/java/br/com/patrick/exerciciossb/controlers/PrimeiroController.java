b2574a0a5d2f2a5fd61065a8173c2e87a0724b57
Patrickaalves <patrickalvessc@gmail.com>
2023-03-29 16:07:27 -0300

    Primeiro webservice e simulando erros

---
+++ src/main/java/br/com/patrick/exerciciossb/controlers/PrimeiroController.java
@@ -0,0 +1,28 @@
+package br.com.patrick.exerciciossb.controlers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PrimeiroController {
+    @RequestMapping(method = RequestMethod.GET, path = "/ola")
+    // @RequestMapping(method = RequestMethod.GET, path = {"/ola", "/ola2"}) Ser chamado em mais de um lugar
+    //@GetMapping("ola") Pode ussar assim tambem
+    public String ola (){
+        return "Ola Spring boot";
+    }
+
+    //@RequestMapping(method = RequestMethod.GET, path = "/ola") Não posso apontar outro metodo para o caminho ola
+    // Se não ira dar erro
+    public String saudaco (){
+        return "Ola Spring boot";
+    }
+
+    @PostMapping(path = "/ola")
+    public String sau (){
+        return "Ola Spring boot POST";
+    }
+
+
+
+
+}
