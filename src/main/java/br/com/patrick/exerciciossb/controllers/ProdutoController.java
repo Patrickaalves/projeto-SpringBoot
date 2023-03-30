52ece01acad9eb3c676a1104d149f6ff939e4f2b
Patrickaalves <patrickalvessc@gmail.com>
2023-03-30 11:27:15 -0300

    Implementando metodos do crud

--- src/main/java/br/com/patrick/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/patrick/exerciciossb/controllers/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -16,5 +17,37 @@ public class ProdutoController {
     public @ResponseBody Produto novoProduto(@Valid Produto produto){
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping
+    public Iterable<Produto> obterProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/pagina/{numeroPagina}")
+    public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina){
+
+
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> obterProdutosId(@PathVariable int id){
+        return produtoRepository.findById(id);
+    }
+
+    @PutMapping
+    public Produto alterraProduto(@Valid Produto produto){
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    /*
+    * Para usar a mesma função para o post e put
+    * @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})*/
+
+    @DeleteMapping("/{id}")
+    public void excluirProduto(@PathVariable int id){
+        produtoRepository.deleteById(id);
     }
 }
